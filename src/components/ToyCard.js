15a239ebf623f9f51702686e10a76be160215703
Ben <benbperry1@gmail.com>
2021-12-09 20:18:52 -0800

    testing

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyCard() {
+function ToyCard({toy, handleDelete}) {
+
+  const {name, image, likes, id} = toy
+  const [addLikes, setAddLikes] = useState(likes)
+
+  function handleLikes() {
+    setAddLikes(addLikes + 1)
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{addLikes} Likes </p>
+      <button className="like-btn" onClick={handleLikes}>Like {"<3"}</button>
+      <button className="del-btn" onClick={()=>handleDelete(id)}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({handleAddToy, toys}) {
+
+  // const { id, name, image} = toys
+
+  const [newToyName, setNewToyName] = useState('')
+  const [newImage, setNewImage] = useState('')
+
+function handleNewToyName(event){
+  setNewToyName(event.target.value)
+  
+}
+
+function handleNewImage(event){
+  setNewImage(event.target.value)
+}
+
+function handleSubmit(event){
+  event.preventDefault()
+  handleAddToy({
+    id: toys.length + 1,
+    name: newToyName,
+     image: newImage,
+     likes: 0,
+    })
+  setNewToyName('')
+  setNewImage('')
+}
+
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleNewToyName}
         />
         <br />
         <input
@@ -17,6 +46,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleNewImage}
         />
         <br />
         <input
